ba946d0ecd70e9a8bdac9ddbed0374f07b52e0f9
J/A <archer884@gmail.com>
2017-06-14 14:46:13 -0500

    Update bitreel

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 extern crate bitreel;
-extern crate hyper;
-extern crate hyper_native_tls;
+extern crate reqwest;
 
 use bitreel::client::*;
 use bitreel::video::*;
 use std::fs::File;
 
 fn main() {
-    let client = get_client();
     let youtube_client = YoutubeClient::new();
 
     if let Some((id, format)) = get_query() {
-        match youtube_client.query(&id, &client) {
+        match youtube_client.query(&id, &ReqwestConnector) {
             Err(e) => {
                 println!("{}", e);
                 std::process::exit(1);
@@ -22,7 +20,7 @@ fn main() {
 
                 let url = video.format(&format).expect("format unavailable");
                 let mut file = File::create(&id).expect("can't save file");
-                let mut res = client.get(url).send().expect("resource not found");
+                let mut res = reqwest::get(url).expect("resource not found");
 
                 io::copy(&mut res, &mut file).expect("unable to write to file");
             }
@@ -38,12 +36,15 @@ fn get_query() -> Option<(String, String)> {
     left.and_then(|x| right.map(|y| (x, y)))
 }
 
-fn get_client() -> hyper::Client {
-    use hyper::net::HttpsConnector;
-    use hyper_native_tls::NativeTlsClient;
+struct ReqwestConnector;
 
-    let ssl = NativeTlsClient::new().unwrap();
-    let connector = HttpsConnector::new(ssl);
-    
-    hyper::Client::with_connector(connector)
+impl ClientConnector for ReqwestConnector {
+    type Err = reqwest::Error;
+    fn download_string(&self, uri: &str) -> Result<String, Self::Err> {
+        use std::io::Read;
+        let mut response = reqwest::get(uri)?;
+        let mut buf = String::new();
+        response.read_to_string(&mut buf).unwrap();
+        Ok(buf)
+    }
 }
